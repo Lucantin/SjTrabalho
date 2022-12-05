ffd127bbddfac777c440a3bbfbdef010087cb8dd
Lucantin <luccabento@gmail.com>
2022-12-05 20:27:05 -0300

    created about me

--- src/pages/aboutMe/index.tsx
+++ src/pages/aboutMe/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import * as S from "./styles";
 
 const AboutMe = () => {
   return (
-    <div>
-      <h1>Sobre</h1>
-      <Link to="/">retornar a página inicial</Link>
-    </div>
+    <S.Wrapper>
+      <S.Title>Lucca Bento</S.Title>
+      <S.Container>
+        <S.SubTitle>
+          Lorem ipsum dolor sit amet. Non maxime unde sed Quis eveniet qui sint
+          vero. Et dolorem maxime est doloribus eligendi vel alias similique in
+          asperiores dolores ut tempore omnis et explicabo tempore. Et iure
+          rerum vel facere facere vel sunt dolore et maxime eius. Et dolor nobis
+          et laborum corrupti ex nisi officiis ut quae tempore! Et autem alias
+          et dolore error et atque totam sit odio quos. Sed modi ipsum est
+          consectetur sint et ratione assumenda sit sequi quaerat et
+          voluptatibus voluptatibus sed animi iusto qui magnam architecto.
+        </S.SubTitle>
+      </S.Container>
+    </S.Wrapper>
   );
 };
 
--- src/pages/aboutMe/styles.ts
+++ src/pages/aboutMe/styles.ts
@@ -1,1 +1,28 @@
 import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 10%;
+`;
+
+export const Container = styled.div`
+  width: 25%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 1.5;
+  border-radius: 10px;
+  border: 3px solid #000;
+  box-shadow: 1px 1px 1px #999;
+`;
+
+export const Title = styled.h1`
+  font-size: 25px;
+  margin-bottom: 10px;
+`;
+
+export const SubTitle = styled.h2``;
