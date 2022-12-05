2d44f27eea0ec4d76469da756519b8f3e71ebe88
Lucantin <luccabento@gmail.com>
2022-12-05 19:50:03 -0300

    button and button style add

---
+++ src/components/button/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export const ButtonGeneral = () => {
+  return (
+    <div className="wrapper">
+      <button>
+        <span>Enviar</span>
+      </button>
+    </div>
+  );
+};
