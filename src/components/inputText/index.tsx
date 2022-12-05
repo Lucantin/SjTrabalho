8f383d675a59d5ee2623b6bbc09d054af90cfdf4
Lucantin <luccabento@gmail.com>
2022-12-05 19:50:42 -0300

    created input text

---
+++ src/components/inputText/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles";
+
+export const InputText = () => {
+  return (
+    <S.Container>
+      <S.InputField />
+    </S.Container>
+  );
+};
---
+++ src/components/inputText/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const InputField = styled.input`
+  resize: none;
+  padding: 10px;
+  width: 100%;
+  line-height: 1.5;
+  border-radius: 10px;
+  border: 3px solid #000;
+  box-shadow: 1px 1px 1px #999;
+`;
