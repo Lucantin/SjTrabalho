00a323c09c7864d5fcbb91b503254677f76daa98
Lucantin <luccabento@gmail.com>
2022-12-05 20:26:34 -0300

    cube in home

---
+++ src/components/homeCube/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+export const Cube = () => {
+  return (
+    <div className="cube">
+      <div className="top"></div>
+      <div>
+        <span className="a"></span>
+        <span className="b"></span>
+        <span className="c"></span>
+        <span className="d"></span>
+      </div>
+    </div>
+  );
+};
